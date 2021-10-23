06e12872bebfd5eb18f25f5e7d9b75727d8b3744
hai061898 <truonghaiauto98@gmail.com>
2021-10-23 22:18:58 +0700

    sp

---
+++ pkg/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import(
+		"github.com/jinzhu/gorm"
+		_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+
+var (
+	db * gorm.DB
+)
+
+func Connect(){
+	d, err := gorm.Open("mysql","")
+	if err != nil {
+		panic(err)
+	}
+	db = d 
+}
+
+func GetDB() *gorm.DB{
+	return db
+}
+
+
